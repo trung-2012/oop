c59c7af6f00b6c21c0c53f6d167279e76f31fafa
trung-2012 <phamtrungasd2012@gmail.com>
2025-09-20 23:04:13 +0700

    Create Introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,17 @@
+package OOP.HW1;
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Phạm Văn Trung"
+                + '\t' + "24021649"
+                + '\t' + "CS5"
+                + '\t' + "PhamVanTrung"
+                + '\t' + "24021649@vnu.edu.vn"
+        );
+        for (int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+            System.out.println("No more bottles of beer on the wall.");
+        }
+    }
+}
