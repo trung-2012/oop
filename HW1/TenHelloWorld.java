eab12a9b1d4fb5a4aec4095eaf5b5285601b2879
trung-2012 <phamtrungasd2012@gmail.com>
2025-09-21 14:05:17 +0700

    Create TenHelloWorld.java

---
+++ HW1/TenHelloWorld.java
@@ -0,0 +1,9 @@
+package OOP.HW1;
+
+public class TenHelloWorld {
+    public static void main(String[] args){
+        for(int i=0;i<10;i++){
+            System.out.println("hello world");
+        }
+    }
+}
