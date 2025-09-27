e29e72b9fa19813d43b419380789328d314c80be
trung-2012 <phamtrungasd2012@gmail.com>
2025-09-27 13:58:02 +0700

    Create Intersect.java

---
+++ HW2/Intersect.java
@@ -0,0 +1,23 @@
+package OOP.HW2;
+
+import java.util.HashSet;
+
+public class Intersect {
+    public static boolean stringIntersect(String a, String b, int len) {
+        if(a==null||b==null||len < 0) return false;
+        if(a.length() < len || b.length() < len) return false;
+
+        HashSet<String> substrings = new HashSet<>();
+
+        for(int i = 0; i <= a.length()-len; i++) {
+            substrings.add(a.substring(i, i+len));
+        }
+        for(int i = 0; i <= b.length()-len; i++) {
+            if(substrings.contains(b.substring(i, i+len))){
+                return true;
+            }
+        }return false;
+
+    }
+    
+}
